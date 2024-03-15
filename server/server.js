08eff5fbc83162a13f55907c0af805fb7b3fee16
Nabin Hamal <nabinhamal2@gmail.com>
2024-03-15 10:09:29 +0545

    usermodel and postmodel created

---
+++ server/server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import dotenv from "dotenv";
+import connectDB from './db/connectDB.js';
+
+
+dotenv.config();
+
+connectDB();
+const app = express();
+
+
+const PORT = process.env.PORT || 5000
+
+
+app.listen(PORT, () => console.log(`RUNNING ON PORT: ${PORT}`))
+
