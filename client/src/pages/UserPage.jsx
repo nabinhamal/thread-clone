ce5ae4d685e0d62a6739657b2145f0fffc6e3fec
Nabin Hamal <nabinhamal2@gmail.com>
2024-03-15 08:31:06 +0545

    fixed error

--- client/src/pages/UserPage.jsx
+++ client/src/pages/UserPage.jsx
@@ -1,8 +1,17 @@
+import UserHeader from "../components/UserHeader"
+import UserPost from "../components/UserPost"
 
 
 const UserPage = () => {
   return (
-    <div>UserPage</div>
+    <>
+   <UserHeader/>
+   <UserPost likes={1200} replies={482} postImage="/post1.png" postTitle="adsad asds" />
+   <UserPost likes={1200} replies={482} postImage="/post2.png" postTitle="adsad asds" />
+   <UserPost likes={1200} replies={482} postImage="/post3.png" postTitle="adsad asds" />
+   <UserPost likes={1200} replies={482} postImage="/post4.png" postTitle="adsad asds" />
+  
+    </>
   )
 }
 
