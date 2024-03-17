2a2cfc4b7d0af3b1f15aacd92e6a8e895a04ff80
Nabin Hamal <nabinhamal2@gmail.com>
2024-03-17 10:04:15 +0545

    like,reply,feed post controller created

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -69,4 +69,88 @@ const deletePost = async (req, res) => {
 }
 
 
-export {createPost,getPost,deletePost}
+const likeUnlikePost = async (req, res) => {
+    try {
+        const {id:postId} = req.params;
+        const userId = req.user.id;
+
+        const post = await Post.findById(postId);
+
+if(!post) {
+    return res.status(404).json({message:"Post not found"})
+}
+
+const userLikedPost = post.likes.includes(userId)
+
+if(userLikedPost){
+     //unlike post
+     await Post.updateOne({_id: postId}, {$pull:{likes: userId}} )
+     res.status(200).json({message:"Post Unliked successfully"})
+}else{
+//like post
+post.likes.push(userId)
+await post.save()
+     res.status(200).json({message:"Post liked successfully"})
+}
+
+    } catch (err) {
+        res.status(500).json({message: err.message});
+        console.log(err);
+        
+    }
+}
+
+const replyToPost = async(req, res) => {
+    try {
+        const{text} = req.body;
+        const postId = req.params.id
+        const userId = req.user._id;
+        const userProfilePic = req.body.profilePic
+        const username = req.user.username
+
+        if(!text){
+            return res.status(400).json({message:"Text field is required"})
+        }
+
+        const post = Post.findById(postId)
+        if(!post){
+            return res.status(400).json({message:"Post not found"})
+        }
+
+        const reply = {userId,text,username,userProfilePic}
+
+        post.replies.push(reply)
+        await post.save()
+
+        return res.status(200).json({message:"Reply added successfully"},post)
+        
+    } catch (err) {
+        res.status(500).json({message: err.message});
+        console.log(err);
+    }
+}
+
+
+const getFeedPosts = async (req, res) => {
+    try {
+        const userId = req.user._id;
+        const user = await User.findById(userId);
+
+        if(!user) {
+            return res.status(404).json({message:"User not found"})
+        }
+
+        const following  = user.following
+
+        const feedPosts = await Post.find({postedBy:{$in:following}}).sort({createdAt: -1})
+
+        res.status(200).json({feedPosts})
+
+
+    } catch (err) {
+        res.status(500).json({message: err.message});
+        console.log(err);
+    }
+}
+
+export {createPost,getPost,deletePost,likeUnlikePost,replyToPost,getFeedPosts}
--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,12 +1,15 @@
 import express from 'express';
-import { createPost, deletePost, getPost } from '../controllers/postController.js';
+import { createPost, deletePost, getFeedPosts, getPost, likeUnlikePost, replyToPost } from '../controllers/postController.js';
 import protectRoute from '../middlewares/protectRoute.js';
 
 
 const router = express.Router();
 
 router.post('/create',protectRoute ,createPost)
+router.post('/like/:id',protectRoute ,likeUnlikePost)
+router.post('/reply/:id',protectRoute ,replyToPost)
 router.get('/:id',getPost)
+router.get('/feed',protectRoute,getFeedPosts)
 router.delete('/:id',protectRoute,deletePost)
 
 
