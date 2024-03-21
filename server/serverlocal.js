11b08fe5ee707c337e98a091819dd770b62f9bfb
Nabin Hamal <nabinhamal2@gmail.com>
2024-03-21 10:10:13 +0545

    .

---
+++ server/serverlocal.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import dotenv from "dotenv";
+import connectDB from './db/connectDB.js';
+import cookieParser from 'cookie-parser';
+import userRoutes from './routes/userRoutes.js';
+import postRoutes from './routes/postRoutes.js';
+import {v2 as cloudinary} from 'cloudinary'
+
+dotenv.config();
+
+connectDB();
+const app = express();
+
+
+const PORT = process.env.PORT || 5000;
+
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+})
+
+app.use(express.json({limit: "2mb"})); //to parse json data in req.body
+app.use(cookieParser()); 
+app.use(express.urlencoded({extended: true}));  //to parse the data in req.body 
+
+//Routes
+app.use("/api/users",userRoutes)
+app.use("/api/posts",postRoutes)
+
+app.listen(PORT, () => console.log(`RUNNING ON PORT: ${PORT}`))
+
